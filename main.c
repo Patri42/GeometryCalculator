82a6faf4989a7e7813550932b8d9f4bf1e2aac5b
Patri42 <patrick.borzych@gmail.com>
2023-04-10 18:45:00 +0200

    Adding headers to maintain shape calculations

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "shape_handlers.h"
 #include "shapes.h"
 #include "calculator.h"
 
@@ -158,81 +159,14 @@ void clear_screen() {
     #endif
 }
 
-void handle_rectangle() {
-    double length, width;
-    printf("Enter the length of the rectangle: ");
-    scanf("%lf", &length);
-    getchar();
-    printf("Enter the width of the rectangle: ");
-    scanf("%lf", &width);
-    getchar();
 
-    double area = rectangle_area(length, width); // Calls the function from shapes.c
-    double circumference = rectangle_circumference(length, width); 
-
-    print_area_circumference(area, circumference);
-}
-
-void handle_parallelogram() {
-    double base, height, side;
-    printf("Enter the base of the parallelogram: ");
-    scanf("%lf", &base);
-    getchar(); 
-    printf("Enter the height of the parallelogram: ");
-    scanf("%lf", &height);
-    getchar(); 
-
-    double area = parallelogram_area(base, height); 
-
-    printf("Enter the side of the parallelogram: ");
-    scanf("%lf", &side);
-    getchar(); 
-
-    double circumference = parallelogram_circumference(base, side); 
-
-    print_area_circumference(area, circumference);
-}
-
-void handle_triangle() {
-    double base, height, side1, side2;
-    printf("Enter the base of the triangle: ");
-    scanf("%lf", &base);
-    getchar(); 
-    printf("Enter the height of the triangle: ");
-    scanf("%lf", &height);
-    getchar(); 
-
-    double area = triangle_area(base, height); 
-
-    printf("Enter the first lateral side: ");
-    scanf("%lf", &side1);
-    getchar(); 
-    printf("Enter the second lateral side: ");
-    scanf("%lf", &side2);
-    getchar(); 
-
-    double circumference = triangle_circumference(base, side1, side2); 
-
-    print_area_circumference(area, circumference);
-}
-
-void handle_circle() {
-    double radius;
-    printf("Enter the radius of the circle: ");
-    scanf("%lf", &radius);
-    getchar(); 
-
-    double area = circle_area(radius); 
-    double circumference = circle_circumference(radius); 
-
-    print_area_circumference(area, circumference);
-}
 
 void print_area_circumference(double area, double circumference) {
     printf("Area: %.2lf\n", area);
     printf("Circumference: %.2lf\n", circumference);
 }
-// / Similarly, create functions handle_parallelogram(), handle_triangle(), and handle_circle()
+
+// Adjust void calculator_menu()
 
 /*To make input validation more robust, consider using fgets() and sscanf() instead of scanf(). 
 This will help you prevent buffer overflows and provide better error handling. 
---
+++ shape_handlers.h
@@ -0,0 +1,9 @@
+#ifndef SHAPE_HANDLERS_H
+#define SHAPE_HANDLERS_H
+
+void handle_rectangle();
+void handle_parallelogram();
+void handle_triangle();
+void handle_circle();
+
+#endif
